742a0008c945d71d0ca9ffb74b5c048579cc886b
Brett Blashko <blashko.brett@gmail.com>
2022-12-04 08:08:36 +0000

    updated defualt theme to lynxlabs

--- searx/settings_defaults.py
+++ searx/settings_defaults.py
@@ -180,7 +180,7 @@ SCHEMA = {
         'static_path': SettingsDirectoryValue(str, os.path.join(searx_dir, 'static')),
         'static_use_hash': SettingsValue(bool, False),
         'templates_path': SettingsDirectoryValue(str, os.path.join(searx_dir, 'templates')),
-        'default_theme': SettingsValue(str, 'simple'),
+        'default_theme': SettingsValue(str, 'lynxlabs', 'UI_DEFAULT_THEME'),
         'default_locale': SettingsValue(str, ''),
         'theme_args': {
             'simple_style': SettingsValue(SIMPLE_STYLE, 'auto'),
